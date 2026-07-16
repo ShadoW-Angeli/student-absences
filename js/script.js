8dbb86991e37d0957981f4ff008a2e0842b8f111
ShadoW-Angeli <angelinamarcova342@gmail.com>
2026-07-16 19:12:29 +0300

    design version 1.0

--- js/script.js
+++ js/script.js
@@ -52,12 +52,14 @@ function createRow(){
             const td3 = document.createElement("td");
             const td4 = document.createElement("td");
             const tdHours = document.createElement("td");
+            tdHours.classList.add("tdHour");
 
             const check = [td1, td2, td3, td4];
             for(let i = 0; i < check.length; i++){
             const checkbox = document.createElement("input");
             checkbox.disabled = !editMode;
             checkbox.type = "checkbox";
+            checkbox.classList.add("checkbox");
 
             let some = "pair" + (i + 1);
             checkbox.checked = element[some];
@@ -95,6 +97,7 @@ function createRow(){
 
             const tdNote = document.createElement("td");
             const inp = document.createElement("input");
+            inp.classList.add("text");
             inp.disabled = !editMode;
             inp.type = "text";
             inp.value = element.note;
@@ -104,11 +107,16 @@ function createRow(){
             })
             tdNote.append(inp);
 
+            const label = document.createElement("label");
+            label.textContent = "Додати файл";
+            label.classList.add("file");
             const tdAdd = document.createElement("input");
             tdAdd.type = "file";
+            label.append(tdAdd);
 
             const tdDell = document.createElement("td");
             const dell = document.createElement("button");
+            dell.classList.add("delet");
             dell.disabled = !editMode;
             dell.textContent = "Видалити";
             dell.addEventListener("click", function(){
@@ -117,7 +125,7 @@ function createRow(){
             });
             tdDell.append(dell);
 
-            tr.append(tdNumber, tdName, td1, td2, td3, td4, tdHours, tdReason, tdNote, tdAdd, tdDell);
+            tr.append(tdNumber, tdName, td1, td2, td3, td4, tdHours, tdReason, tdNote, label, tdDell);
         })
     }
     editMode = true;
@@ -170,14 +178,18 @@ clean.addEventListener("click", function(){
     task = [];
     renderTable();
 })
-
+saveChange.classList.remove("hidden");
 saveChange.addEventListener("click", function(){
     editMode = false;
     renderTable();
+   // saveChange.classList.add("hidden");
 });
+edit.classList.remove("hidden");
 edit.addEventListener("click", function(){
     editMode = true;
     renderTable();
+   // saveChange.classList.remove("hidden");
+   // edit.classList.add("hidden");
 });
 
 sort.addEventListener("click", function(){
